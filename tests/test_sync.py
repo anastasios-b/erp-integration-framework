0509adf52f33b9d0f1e2728c8b1123be2e3b7483
Anastasios B. <bolkas.anastasios@gmail.com>
2026-01-13 21:39:37 +0200

    Update sys.path to include only parent directory

--- tests/test_sync.py
+++ tests/test_sync.py
@@ -7,7 +7,7 @@ import sys
 import os
 
 # Add src directory to path for imports
-sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'src'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 from src.field_mapper import FieldMapper
 from src.validator import ProductValidator
